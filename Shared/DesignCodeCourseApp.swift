0e272ecbcd775948558067d9a4834e8ba8071a64
Yuri Stanislavsky <stanislavskiy@me.com>
2020-07-30 14:36:47 +0300

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Юрий Станиславский on 30.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
