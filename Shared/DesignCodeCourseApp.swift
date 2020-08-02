de7e997506ebbc8770143130aeae85ca382d5286
Yuri Stanislavsky <stanislavskiy@me.com>
2020-08-02 20:40:22 +0300

    Done: Toolbar and Organizing Cross-platform buttons and content for navigation, status and actions

--- Shared/DesignCodeCourseApp.swift
+++ Shared/DesignCodeCourseApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct DesignCodeCourseApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Sidebar()
+//            ContentView()
         }
     }
 }
