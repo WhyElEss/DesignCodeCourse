f2ea50a1379590be7a51c09f168ef93a9e12060e
Yuri Stanislavsky <stanislavskiy@me.com>
2020-08-10 17:41:03 +0300

    Done: Size Class and Tab Bar Setting the Tab Bar for iPhone versus Sidebar for iPad

--- Shared/DesignCodeCourseApp.swift
+++ Shared/DesignCodeCourseApp.swift
@@ -11,7 +11,14 @@ import SwiftUI
 struct DesignCodeCourseApp: App {
     var body: some Scene {
         WindowGroup {
+            #if os(iOS)
             ContentView()
+            #else
+            ContentView()
+                .onAppear {
+                    NSApp.keyWindow?.toolbar?.insertItem(withItemIdentifier: .toggleSidebar, at: 0)
+                }
+            #endif
         }
     }
 }
